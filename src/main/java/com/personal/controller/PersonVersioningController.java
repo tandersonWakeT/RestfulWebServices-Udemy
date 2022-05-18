fe6e1e01f5b6096ec1c225bc230de994ad3097bc
tandersonWakeT <tsanderson2@my.waketech.edu>
2022-05-18 17:31:16 -0400

    comments Header Versioning - breaks OpenAPI

--- src/main/java/com/personal/controller/PersonVersioningController.java
+++ src/main/java/com/personal/controller/PersonVersioningController.java
@@ -63,15 +63,18 @@ public class PersonVersioningController {
 	}
 	
 	// ==================== Headers ====================
-	@GetMapping(value="/person/header", headers="X-API-VERSION=1")
-	public PersonV1 headerV1() {
-		return new PersonV1("Bob Charlie");
-	}
 	
-	@GetMapping(value="/person/header", headers="X-API-VERSION=2")
-	public PersonV2 headerV2() {
-		return new PersonV2(new Name("Scarlett", "Johanson"));
-	}
+	// **NOTE** Headers known issue which breaks OpenAPI
+	
+//	@GetMapping(value="/person/header", headers="X-API-VERSION=1")
+//	public PersonV1 headerV1() {
+//		return new PersonV1("Bob Charlie");
+//	}
+//	
+//	@GetMapping(value="/person/header", headers="X-API-VERSION=2")
+//	public PersonV2 headerV2() {
+//		return new PersonV2(new Name("Scarlett", "Johanson"));
+//	}
 	
 	// ==================== Produces ====================
 	@GetMapping(value="/person/produces", produces="application/vnd.company.app-v1+json")
